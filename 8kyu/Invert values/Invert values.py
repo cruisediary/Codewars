0badfa9a3f81bac505af3fa449eebc94266d0beb
Cruz <cruzdiary@gmail.com>
2020-03-08 14:11:17 +0900

    Solve Invert values 8kyu

---
+++ 8kyu/Invert values/Invert values.py
@@ -0,0 +1,28 @@
+"""
+
+https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/python
+
+Invert values
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+"""
+
+def invert(lst):
+    return list(map(lambda x: -1 * x, lst))
+
+"""
+
+Sample Tests
+
+Test.it("Basic Tests")
+Test.assert_equals(invert([1,2,3,4,5]),[-1,-2,-3,-4,-5])
+Test.assert_equals(invert([1,-2,3,-4,5]), [-1,2,-3,4,-5])
+Test.assert_equals(invert([]), [])
+
+"""
