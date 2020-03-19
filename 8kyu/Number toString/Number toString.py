0656ad5a355ac800a0fa1045e15e1f742468ad8f
Cruz <cruzdiary@gmail.com>
2020-03-19 22:23:59 +0900

    Solve Number toString 8kyu

---
+++ 8kyu/Number toString/Number toString.py
@@ -0,0 +1,21 @@
+"""
+https://www.codewars.com/kata/53934feec44762736c00044b/train/python
+
+Number toString
+
+The code gives an error!
+
+a = 123.toString()
+Fix it!
+
+"""
+
+a = str(123)
+
+"""
+
+Sample Tests
+
+test.assert_equals(a, '123', 'Wrong!')
+
+"""
